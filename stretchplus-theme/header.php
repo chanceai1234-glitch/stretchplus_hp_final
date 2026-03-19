d65011a699d078464d83afe62689fdafed08ac66
chanceai1234-glitch <chance.ai1234@gmail.com>
2026-03-19 23:26:46 +0900

    fix(core): restore template_directory_uri binding to stylesheet in header.php

--- stretchplus-theme/header.php
+++ stretchplus-theme/header.php
@@ -13,7 +13,7 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="style.css?cb=1773901146611">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?cb=1773901146611">
     <?php wp_head(); ?>
 </head>
 
